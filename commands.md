bad089e1f21b3fc17570fd8d44af0de8b9025281
Govind Chandra <govind.chandra@jic.ac.uk>
2018-12-12 17:05:46 +0000

    JAC

--- commands.md
+++ commands.md
@@ -2,4 +2,53 @@
 
 #### Tue 04 Dec 2018
 
+Hi Natasha,
+
+inframeTTA.pl is attached.
+
+Run it as
+
+  perl inframeTTA.pl sco_plasmids.gbk
+
+I have attached sco_plasmids.gbk for testing.
+
+You should get a tabular output on stdout as well as the files out.fna
+and out.faa. These are fasta files containing the nucleotide and
+protein sequences of the TTA containing genes.
+
+In the tabular output
+
+Column 1: is the gene identifier
+
+Column 2: is the position of the TTA codon in the nucleotide sequence
+of the gene. Note that this position is zero based.
+
+Column 3: is the value of the "product" tag in the annotation for the gene.
+
+
+As you can probably guess from the script itself, you may specify more
+than one genbank files on the command line.
+
+Let me know whether the script works for you or not. If it does not,
+please paste any error messages you get in an email to me.
+
+
+I have deliberately kept the script very simple i.e. haven't used
+command line options processing or complex error reporting. Of course,
+the complexity coming from using BioPerl (Bio::SeqIO) cannot be
+avoided. The idea is to test a very minimal script before going any
+further.
+
+Best wishes.
+
+Govind
+
+
+
+
+~~~ {.sh}
+
+zip -j -r ~/mnt/wstemp/natasha/ttaScriptAndTestInput code/inframeTTA.pl sco_plasmids.gbk
+
+~~~
 
--- inframeTTA.pl
+++ inframeTTA.pl
@@ -1,199 +1,102 @@
-#!/usr/bin/perl
-use 5.14.0;
-use utf8;
-use Carp;
-use lib qw(/home/sco /home/sco/mnt/smoke/perllib);
-use File::Basename;
-use Sco::Common qw(tablist linelist tablistE linelistE tabhash tabhashE tabvals
-    tablistV tablistVE linelistV linelistVE tablistH linelistH
-    tablistER tablistVER linelistER linelistVER tabhashER tabhashVER csvsplit);
-use File::Spec;
+# use 5.14.0;
+use strict;
 use Bio::SeqIO;
 
-# {{{ Getopt::Long
-use Getopt::Long;
-my $outdir;
-my $indir;
-my $fofn;
-my $outex; # extension for the output filename when it is derived on infilename.
-my $conffile = qq(local.conf);
-my $errfile;
-my $runfile;
-my $outfile;
-my $testCnt = 0;
-our $verbose;
-my $skip = 0;
-my $help;
-GetOptions (
-"outfile:s" => \$outfile,
-"dirout:s" => \$outdir,
-"indir:s" => \$indir,
-"fofn:s" => \$fofn,
-"extension:s" => \$outex,
-"conffile:s" => \$conffile,
-"errfile:s" => \$errfile,
-"runfile:s" => \$runfile,
-"testcnt:i" => \$testCnt,
-"skip:i" => \$skip,
-"verbose" => \$verbose,
-"help" => \$help
-);
-# }}}
+my @infiles = @ARGV;
 
+my $fnafn = "out.fna";
+my $faafn = "out.faa";
 
-if($help) {
-exec("perldoc $0");
-exit;
+# fasta output for nucleotide sequences
+my $ntout;
+if($fnafn) {
+  $ntout = Bio::SeqIO->new(-file => ">$fnafn", -format => "fasta");
 }
 
-# {{{ open the errfile
-if($errfile) {
-open(ERRH, ">", $errfile);
-print(ERRH "$0", "\n");
-close(STDERR);
-open(STDERR, ">&ERRH"); 
-}
-# }}}
-
-# {{{ Populate %conf if a configuration file 
-my %conf;
-if(-s $conffile ) {
-  open(my $cnfh, "<", $conffile);
-  my $keyCnt = 0;
-  while(my $line = readline($cnfh)) {
-    chomp($line);
-    if($line=~m/^\s*\#/ or $line=~m/^\s*$/) {next;}
-    my @ll=split(/\s+/, $line, 2);
-    $conf{$ll[0]} = $ll[1];
-    $keyCnt += 1;
-  }
-  close($cnfh);
-  linelistE("$keyCnt keys placed in conf.");
-}
-elsif($conffile ne "local.conf") {
-linelistE("Specified configuration file $conffile not found.");
-}
-# }}}
-
-# {{{ outdir and outfile business.
-my $ofh;
-my $idofn = 0;    # Flag for input filename derived output filenames. 
-if($outfile) {
-  my $ofn;
-  if($outdir) {
-    unless(-d $outdir) {
-      unless(mkdir($outdir)) {
-        croak("Failed to make $outdir. Exiting.");
-      }
-    }
-    $ofn = File::Spec->catfile($outdir, $outfile);
-  }
-  else {
-    $ofn = $outfile;
-  }
-  open($ofh, ">", $ofn);
-}
-elsif($outdir) {
-linelistE("Output filenames will be derived from input");
-linelistE("filenames and placed in $outdir");
-    unless(-d $outdir) {
-      unless(mkdir($outdir)) {
-        croak("Failed to make $outdir. Exiting.");
-      }
-    }
-$idofn = 1;
-}
-else {
-  open($ofh, ">&STDOUT");
-}
-select($ofh);
-# }}}
-
-# {{{ populate @infiles
-my @infiles;
-if(-e $fofn and -s $fofn) {
-open(FH, "<", $fofn);
-while(my $line = readline(FH)) {
-chomp($line);
-if($line=~m/^\s*\#/ or $line=~m/^\s*$/) {next;}
-my $fn;
-if($indir) {
-$fn = File::Spec->catfile($indir, $line);
-}
-else {
-$fn = $line;
+# fasta output for protein sequences
+my $aaout;
+if($faafn) {
+  $aaout = Bio::SeqIO->new(-file => ">$faafn", -format => "fasta");
 }
 
-push(@infiles, $fn);
-}
-close(FH);
-}
-else {
-@infiles = @ARGV;
-}
 
-# }}}
-
+# Loop through all CDSs inside each sequence object inside each genbank file.
+# A single genbank file can have multiple sequences in it.
 
 for my $infile (@infiles) {
-
-my $seqio=Bio::SeqIO->new(-file => $infile);
-# my $seqout=Bio::SeqIO->new(-fh => $ofh, -format => 'fasta');
-
-
-while(my $seqobj=$seqio->next_seq()) {
-my $cdsCnt = 0;
-  for my $feature ($seqobj->all_SeqFeatures()) {
-    if($feature->primary_tag() eq 'CDS') {
-      $cdsCnt += 1;
-      my $codon_start = 1;
-      if($feature->has_tag('codon_start')) {
-        ($codon_start) = $feature->get_tag_values('codon_start');
-      }
-
-      my %anno;
-      for my $tag (qw(gene note product locus_tag gene_synonym)) {
-        if($feature->has_tag($tag)) {
-          my @taglist = $feature->get_tag_values($tag);
-          $anno{$tag} = \@taglist;
+  my $seqio=Bio::SeqIO->new(-file => $infile);
+  while(my $seqobj=$seqio->next_seq()) {
+    my $cdsCnt = 0;
+    for my $feature ($seqobj->all_SeqFeatures()) {
+      if($feature->primary_tag() eq 'CDS') {
+        $cdsCnt += 1;
+        my $codon_start = 1;
+        if($feature->has_tag('codon_start')) {
+          ($codon_start) = $feature->get_tag_values('codon_start');
         }
-      }
-      my $subobj = $feature->spliced_seq(-nosort => 1);
-      my $offset = 0;
-      if($codon_start > 1) { $offset += $codon_start - 1; }
-      my @ttapos = inframeTTA($subobj, $offset);
-      if(@ttapos) {
-        my $id;
-        if(exists($anno{locus_tag})) {
-          $id = $anno{locus_tag}->[0];
+        my $offset = 0;
+        if($codon_start > 1) { $offset += $codon_start - 1; }
+        my $subobj = $feature->spliced_seq(-nosort => 1);
+        my @ttapos = inframeTTA($subobj, $offset);
+        if(@ttapos) {
+          my ($id, $product) = idAndProd($feature, $cdsCnt);
+          print(join("\t", $id, join(" ", @ttapos), $product), "\n");
+          if($ntout) {
+            my $featobj=$feature->spliced_seq(-nosort => 1);
+            $featobj->display_id($id);
+            $ntout->write_seq($featobj);
+          }
+          if($aaout) {
+            my $aaobj = featTranslate($feature);
+            $aaobj->display_id($id);
+            $aaout->write_seq($aaobj);
+          }
         }
-        elsif(exists($anno{gene_synonym})) {
-          $id = $anno{gene_synonym}->[0];
-        }
-        elsif(exists($anno{gene})) {
-          $id = $anno{gene}->[0];
-        }
-        else {
-          $id = "CDS_" . sprintf("%04d", $cdsCnt);
-        }
-        if(not exists($anno{product})) { push(@{$anno{product}}, "None"); }
-        print(join("\t", $id, join(" ", @ttapos), @{$anno{product}}), "\n");
       }
     }
   }
 }
-}
-
 
 exit;
 
-# Multiple END blocks run in reverse order of definition.
-END {
-close($ofh);
-close(STDERR);
-close(ERRH);
-}
 
+### subroutines below ###
+
+# {{{ sub idAndProd
+# Returns two strings.
+sub idAndProd {
+  my $feature = shift(@_);
+  my $cdsCnt = shift(@_);
+  my %anno;
+  for my $tag (qw(gene note product locus_tag gene_synonym)) {
+    if($feature->has_tag($tag)) {
+      my @taglist = $feature->get_tag_values($tag);
+      $anno{$tag} = \@taglist;
+    }
+  }
+  my $id;
+  if(exists($anno{locus_tag})) {
+    $id = $anno{locus_tag}->[0];
+  }
+  elsif(exists($anno{gene_synonym})) {
+    $id = $anno{gene_synonym}->[0];
+  }
+  elsif(exists($anno{gene})) {
+    $id = $anno{gene}->[0];
+  }
+  else {
+    $id = "CDS_" . sprintf("%04d", $cdsCnt);
+  }
+  my $pproduct;
+  if(exists($anno{product})) { $pproduct = join(" ", @{$anno{product}}); }
+  else { $pproduct = "None"; }
+  return($id, $pproduct);
+}
+# }}}
+
+
+# {{{ sub inframeTTA
+# Returns a list of (zero based) nucleotide positions.
 sub inframeTTA {
   my $inseq=shift(@_);
   my $offset = shift(@_);
@@ -203,30 +106,30 @@ sub inframeTTA {
   else { $seq = lc($inseq); }
   my $pos = $offset;
   while(my $triplet = substr($seq, $pos, 3)) {
-    if($triplet=~m/[tu][tu]a/i) {
+    if($triplet =~ m/[tu][tu]a/i) {
       push(@retlist, $pos);
     }
-    $pos+=3;
+    $pos += 3;
   }
   return(@retlist);
 }
+# }}}
 
 
 
-# # {{{ sub feat_translate {
-# sub feat_translate {
-#   my $feature=shift(@_);
-#   my $codon_start=1;
-#   if($feature->has_tag('codon_start')) {
-#     ($codon_start) = $feature->get_tag_values('codon_start');
-#   }
-#   my $aaobj;
-#   eval {
-#     my $offset=1;
-#     if($codon_start > 1) { $offset = $codon_start;}
-#     my $featobj=$feature->spliced_seq(-nosort => 1);
-#     $aaobj = $featobj->translate(-offset => $offset, -complete => 1);
-#   };
-#   return($aaobj);
-# }
-# # }}}
+# {{{ sub featTranslate
+# Returns a Bio::Seq
+sub featTranslate {
+  my $feature=shift(@_);
+  my $codon_start=1;
+  if($feature->has_tag('codon_start')) {
+    ($codon_start) = $feature->get_tag_values('codon_start');
+  }
+  my $aaobj;
+  my $frame = 0;
+  if($codon_start > 1) { $frame += ($codon_start-1);}
+  my $featobj = $feature->spliced_seq(-nosort => 1);
+  $aaobj = $featobj->translate(-frame => $frame, -complete => 0);
+  return($aaobj);
+}
+# }}}
